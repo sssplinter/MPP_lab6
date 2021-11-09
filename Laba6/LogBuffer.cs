0d5e7003fe64a29318858cdc76a837755a03fd6d
Kristina <kristinasementsova21@gmaol.com>
2021-11-09 21:06:17 +0300

    lab6 completed

---
+++ Laba6/LogBuffer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Timers;
+
+using Timer = System.Timers.Timer;
+
+namespace Laba6
+{
+    public class LogBuffer
+    {
+        private readonly ConcurrentQueue<string> _bufferQueue = new ConcurrentQueue<string>();
+        private readonly StreamWriter _streamWriter;
+
+        private readonly int _bufLimit;
+
+        public LogBuffer(string filePath, int bufLimit, int timerLimit)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception($"File {filePath} doesn't exists");
+            }
+
+            _streamWriter = new StreamWriter(filePath, true);
+
+            _bufLimit = bufLimit;
+            
+            var timer = new Timer {Interval = timerLimit};
+            timer.Elapsed += CheckTimeInterval;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        public void Add(string msg)
+        {
+            _bufferQueue.Enqueue(msg);
+            CheckBufferLimit();
+        }
+
+        private async void CheckBufferLimit()
+        {
+            if (_bufferQueue.Count < _bufLimit) return;
+            while (!_bufferQueue.IsEmpty)
+            {
+                _bufferQueue.TryDequeue(out var message);
+                if (message != null)
+                {
+                    await _streamWriter.WriteLineAsync(message);
+                }
+            }
+        }
+
+        private async void CheckTimeInterval(object sender, ElapsedEventArgs e)
+        {
+            while (!_bufferQueue.IsEmpty)
+            {
+                _bufferQueue.TryDequeue(out var message);
+                if (message != null)
+                {
+                    await _streamWriter.WriteLineAsync(message);
+                }
+            }
+        }
+
+        public void Close()
+        {
+            _streamWriter.Close();
+        }
+    }
+}
---
+++ Laba6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laba6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logBuffer = new LogBuffer("C:/Users/krist/OneDrive/Рабочий стол/mpp_lab6.txt", 10, 10000);
+            for (var index = 1; index <= 50; index++)
+            {
+                logBuffer.Add(index.ToString());
+            }
+
+            logBuffer.Close();
+        }
+    }
+}
